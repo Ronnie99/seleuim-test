439efd3eb3eb8a81a450aab6c11a36d0117f5b53
ronnie <ronnie6330@163.com>
2017-08-06 16:52:00 +0800

    'update'

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ driver.get("http://www.baidu.com");
 driver.findElement({id:'kw'}).sendKeys('上海地图');
 driver.findElement({css:'#\31 > div > div.c-span12.c-span-last > form > p:nth-child(6) > span > input'}).sendkeys('御桥地铁站');
 driver.findElement({css:'#\31 > div.c-border > div > div.c-row > div.c-span18.c-span-last > div > div:nth-child(2) > form > a'}).click();
-
+driver.quit()
 
 
