9016d55e2ef3e6bcb69c60443d0d2ed48444385b
ronnie <ronnie6330@163.com>
2017-08-09 10:22:05 +0800

    'update'

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 require("chromedriver");
-var webdriver=require('selenium-webdriver')
+var webdriver=require('selenium-webdriver');
 var driver =new webdriver.Builder().forBrowser("chrome").build();
 driver.get("http://www.baidu.com");
 driver.findElement({id:'kw'}).sendKeys('上海地图');
 driver.findElement({id:'su'}).click();
+driver.sleep(10*1000);//等待10秒
+driver.findElement({css:' div > div.c-span12.c-span-last > form > p:nth-child(6) > span > input'}).sendKeys('御桥');
+driver.findElement({css:' div > div.c-span12.c-span-last > form > p.c-gap-top.op-mapcity-btm > a'}).click();
 
